2397ba75b5b85a8286fefeaffd1cc266d2462208
SUKANYA S PEDAMKAR <sukanyapedamkar11@gmail.com>
2018-06-26 15:32:18 -0400

    first commit of portfolio

---
+++ pages/aboutme.php
@@ -0,0 +1,104 @@
+<div id="home">
+    <div class="row profileInfoWrap">
+        <div class="col-lg-4 col-sm-5">
+            <div class="profileImg">
+                <img src="images/profile-Img.jpg" alt="profile-img">
+            </div>
+        </div>
+        <div class="col-lg-8 col-sm-7">
+            <h2>Sukanya <span>Pedamkar</span></h2>
+            <h4>Hi! I am Web Developer. Currently based in Jersey City, New Jersey.</h4>
+            <p>  My passion is to bring ideas into life. And I work earnestly to make it pixel perfect. When I m not writing coding or doing research, you ll find me in Theaters probably watching all awesome movies out there.</p>
+            
+        </div>
+    </div>
+    <div class="row timeline">
+        <div class="col-sm-12">
+            <div class="timelineContainer">
+                <div class="timelineBlock">
+                    <div class="timelineImg">
+                        <img src="images/icon-laptop.png" alt="Picture">
+                    </div>
+
+                    <div class="timelineContent">
+                        <h5 class="title">LumiFi Inc.</h5>
+                        <h6 class="degree"> Product & IT Intern</h6>
+                        <label class="location">New York, NY</label>
+                        <span class="timelineDate">June 2017 - December 2017 </span>
+                    </div>
+                </div> 
+                <div class="timelineBlock">
+                    <div class="timelineImg">
+                        <img src="images/icon-laptop.png" alt="Picture">
+                    </div>
+
+                    <div class="timelineContent">
+                        <h5 class="title">Girls Inc.</h5>
+                        <h6 class="degree"> Web Devloper</h6>
+                        <label class="location">New York, NY</label>
+                        <span class="timelineDate">March 2017 - October 2017 </span>
+                    </div>
+                </div>
+                <div class="timelineBlock">
+                    <div class="timelineImg">
+                        <img src="images/icon-laptop.png" alt="Picture">
+                    </div>
+
+                    <div class="timelineContent">
+                        <h5 class="title">SteamWorks Studio</h5>
+                        <h6 class="degree"> Teacher Assitant</h6>
+                        <label class="location">Jersey City, NJ</label>
+                        <span class="timelineDate">October 2016 - May 2018 </span>
+                    </div>
+                </div> 
+                <div class="timelineBlock">
+                    <div class="timelineImg">
+                        <img src="images/icon-cap.png" alt="Picture">
+                    </div>
+                    <div class="timelineContent">
+                        <h5 class="title">New York Institute of Technology</h5>
+                        <h6 class="degree"> Masters in Computer Science</h6>
+                        <label class="location">New York, NY</label>
+                        <span class="timelineDate">August 2013 - July 2016 </span>
+                    </div>
+                </div> 
+                <div class="timelineBlock">
+                    <div class="timelineImg">
+                        <img src="images/icon-laptop.png" alt="Picture">
+                    </div>
+
+                    <div class="timelineContent">
+                        <h5 class="title">Fractal Ink Design Studio</h5>
+                        <h6 class="degree"> Web Devloper</h6>
+                        <label class="location">Mumbai, MH</label>
+                        <span class="timelineDate">August 2013 - July 2016 </span>
+                    </div>
+                </div> 
+                <div class="timelineBlock">
+                    <div class="timelineImg">
+                        <img src="images/icon-cap.png" alt="Picture">
+                    </div>
+                    <div class="timelineContent">
+                        <h5 class="title">Mumbai University</h5>
+                        <h6 class="degree"> Bachelors of Engineering Information Technology</h6>
+                        <label class="location">Mumbai, MH</label>
+                        <span class="timelineDate">July 2010 - July 2013 </span>
+                    </div>
+
+                </div> 
+                <div class="timelineBlock">
+                    <div class="timelineImg">
+                        <img src="images/icon-cap.png" alt="Picture">
+                    </div>
+                    <div class="timelineContent">
+                        <h5 class="title">Vidyalankar Polytechnic</h5>
+                        <h6 class="degree"> Diploma in Information Technology</h6>
+                        <label class="location">Mumbai, MH</label>
+                        <span class="timelineDate">June 2007 - June 2010 </span>
+                    </div>
+                    
+                </div> 
+            </div>
+        </div>
+    </div>
+</div>
---
+++ pages/projects.php
@@ -0,0 +1,233 @@
+<div id="project" class="projectWrap">
+    <h3>Projects</h3>
+    <p>Here's some of the projects that I have worked on.</p>
+    <div style="display:none;">
+        <div id="ninja-slider">
+            <div class="slider-inner">
+                <ul>
+                    <li>
+                        <a class="ns-img" href="images/projects/jtcpl.jpg"></a>
+                        <div class="caption">
+                            <h6>JTCPL Designs</h6>
+                            <p>Responsive, Boilerplate, Bootstrap, jQuery</p>
+                        </div>
+                    </li>
+                    <li>
+                        <a class="ns-img" href="images/projects/edelweiss.jpg"></a>
+                        <div class="caption">
+                            <h6>Edelweiss</h6>
+                            <p>Reponsive, jQuery, Animation, Slider integration</p>
+                        </div>
+                    </li>
+                    <li>
+                        <a class="ns-img" href="images/projects/cerebrus.jpg"></a>
+                        <div class="caption">
+                            <h6>Cerebrus</h6>
+                            <p>HTML, CSS, jQuery, Plugin integration, Content Management</p>
+                        </div>
+                    </li>
+                    <li>
+                        <a class="ns-img" href="images/projects/excel.jpg"></a>
+                        <div class="caption">
+                            <h6>Excel Industries</h6>
+                            <p>HTML, CSS, jQuery, Masonry and plugin intergration</p>
+                        </div>
+                    </li>
+                    <li>
+                        <a class="ns-img" href="images/projects/eventfaq.jpg"></a>
+                        <div class="caption">
+                            <h6>EventFAQs</h6>
+                            <p>Bootstrap, Responsive, Modular HTML and CSS, jQuery</p>
+                        </div>
+                    </li>
+                    <li>
+                        <a class="ns-img" href="images/projects/lumifi.jpg"></a>
+                        <div class="caption">
+                            <h6>LumiFi Inc.</h6>
+                            <p>Angular JS, Responsive, Q&A testing, JIRA</p>
+                        </div>
+                    </li>
+                    <li>
+                        <a class="ns-img" href="images/projects/girlsincli.jpg"></a>
+                        <div class="caption">
+                            <h6>Girls Inc.</h6>
+                            <p>Wordpress, Responsive, Plugin integration, brainstorm and Client meet</p>
+                        </div>
+                    </li>
+                    <li>
+                        <a class="ns-img" href="images/projects/icici.jpg"></a>
+                        <div class="caption">
+                            <h6>ICICI</h6>
+                            <p>HTMl, CSS, jQuery, Content Management, Client Meet</p>
+                        </div>
+                    </li>
+                    <li>
+                        <a class="ns-img" href="images/projects/hdfc.jpg"></a>
+                        <div class="caption">
+                            <h6>HDFC</h6>
+                            <p>Bootstrap, Modular HTML and CSS and jQuery</p>
+                        </div>
+                    </li>
+                    <li>
+                        <a class="ns-img" href="images/projects/tata.jpg"></a>
+                        <div class="caption">
+                            <h6>Tata International</h6>
+                            <p>Content Management, Image optimization</p>
+                        </div>
+                    </li>
+                    <li>
+                        <a class="ns-img" href="images/projects/syska.jpg"></a>
+                        <div class="caption">
+                            <h6>Syska</h6>
+                            <p>Bootstrap, Responsive</p>
+                        </div>
+                    </li>
+                </ul>
+                <div id="fsBtn" class="fs-icon" title="Expand/Close"></div>
+            </div>
+        </div>
+    </div>
+    <ul class="row">
+        <li class="col-md-6 col-sm-6 col-xs-12 projectList">  
+            <div class="project">
+                <a href="#" class="projectImgWrap" onclick="lightbox(0)">                    
+                    <img src="images/projects/jtcpl.jpg" alt="" class="img">
+                </a>                
+                <div class="projectSnippet">                    
+                    <a href="http://www.jtcpldesigns.com/" target="_blank" class="name">JTCPL designs</a>                    
+                    <label class="type">Responsive, Boilerplate, Bootstrap, jQuery</label>
+                </div>
+            </div>
+        </li>
+        <li class="col-md-6 col-sm-6 col-xs-12 projectList">
+            <div class="project">
+                <a href="#" class="projectImgWrap" onclick="lightbox(1)">
+                    <img src="images/projects/edelweiss.jpg" alt="" class="img">
+                </a>
+                <div class="projectSnippet">
+                    <a href="https://www.edelweisstokio.in/timeline" target="_blank" class="name">Edelweiss Tokio</a>
+                    <label class="type">Reponsive, jQuery, Animation, Slider integration</label>
+                </div>
+            </div>
+        </li>
+        <li class="col-md-6 col-sm-6 col-xs-12 projectList">
+            <div class="project">
+                <a href="#" class="projectImgWrap" onclick="lightbox(2)">
+                    <img src="images/projects/cerebrus.jpg" alt="" class="img">
+                </a>
+                <div class="projectSnippet">
+                    <a href="http://cerebrus-consultants.com/home" target="_blank" class="name">Cerebrus</a>
+                    <label class="type">
+                    HTML, CSS, jQuery, Plugin integration, Content Management</label>
+                </div>
+            </div>
+        </li>
+        <li class="col-md-6 col-sm-6 col-xs-12 projectList">
+            <div class="project">
+                <a href="#" class="projectImgWrap" onclick="lightbox(3)">
+                    <img src="images/projects/excel.jpg" alt="" class="img">
+                </a>
+                <div class="projectSnippet">
+                    <a href="http://excelind.co.in/Excel_ENBT/" target="_blank" class="name">Excel Industries</a>
+                    <label class="type">HTML, CSS, jQuery, Masonry and mscrollbar plugin intergration</label>
+                </div>
+            </div>
+        </li>
+        <li class="col-md-6 col-sm-6 col-xs-12 projectList">
+            <div class="project">
+                <a href="#" class="projectImgWrap" onclick="lightbox(4)">
+                    <img src="images/projects/eventfaq.jpg" alt="" class="img">
+                </a>
+                <div class="projectSnippet">
+                    <a href="https://www.eventfaqs.com/" target="_blank" class="name">EventFAQs</a>
+                    <label class="type">Bootstrap, Responsive, Modular HTML and CSS, jQuery</label>
+                </div>
+            </div>
+        </li>
+        <li class="col-md-6 col-sm-6 col-xs-12 projectList">
+            <div class="project">
+                <a href="#" class="projectImgWrap" onclick="lightbox(5)">
+                    <img src="images/projects/lumifi.jpg" alt="" class="img">
+                </a>
+                <div class="projectSnippet">
+                    <a href="#" class="name">LumiFi Inc.</a>
+                    <label class="type">Angular JS, Responsive, Q&A testing, JIRA</label>
+                </div>
+            </div>
+        </li>
+        <li class="col-md-6 col-sm-6 col-xs-12 projectList">
+            <div class="project">
+                <a href="#" class="projectImgWrap" onclick="lightbox(6)">
+                    <img src="images/projects/girlsincli.jpg" alt="" class="img">
+                </a>
+                <div class="projectSnippet">
+                    <a href="http://girlsincli.org/" target="_blank" class="name">Girls Inc.</a>
+                    <label class="type">Wordpress, Responsive, Plugin integration</label>
+                </div>
+            </div>
+        </li>
+        <li class="col-md-6 col-sm-6 col-xs-12 projectList">
+            <div class="project">
+                <a href="#" class="projectImgWrap" onclick="lightbox(7)">
+                    <img src="images/projects/icici.jpg" alt="" class="img">
+                </a>
+                <div class="projectSnippet">
+                    <a href="https://www.iciciprulife.com/tools-and-calculators.html" target="_blank" class="name">ICICI</a>
+                    <label class="type">HTMl, CSS, jQuery, Content Management, Client Meet</label>
+                </div>
+            </div>
+        </li>
+        <li class="col-md-6 col-sm-6 col-xs-12 projectList">
+            <div class="project">
+                <a href="#" class="projectImgWrap" onclick="lightbox(8)">
+                    <img src="images/projects/hdfc.jpg" alt="" class="img">
+                </a>
+                <div class="projectSnippet">
+                    <a href="https://ifms.hdfcbank.com/delfin/Login.jsp" target="_blank" class="name">HDFC</a>
+                    <label class="type">Bootstrap, Modular HTML and CSS and jQuery</label>
+                </div>
+            </div>
+        </li>
+        <li class="col-md-6 col-sm-6 col-xs-12 projectList">
+            <div class="project">
+                <a href="#" class="projectImgWrap" onclick="lightbox(9)">
+                    <img src="images/projects/tata.jpg" alt="" class="img">
+                </a>
+                <div class="projectSnippet">
+                    <a href="http://tatainternational.com/leather/" target="_blank" class="name">Tata International</a>
+                    <label class="type">Content Management, Image optimization </label>
+                </div>
+            </div>
+        </li>
+        <li class="col-md-6 col-sm-6 col-xs-12 projectList">
+            <div class="project">
+                <a href="#" class="projectImgWrap" onclick="lightbox(10)">
+                    <img src="images/projects/syska.jpg" alt="" class="img">
+                </a>
+                <div class="projectSnippet">
+                    <a href="#" class="name">Syska</a>
+                    <label class="type">Bootstrap, Responsive </label>
+                </div>
+            </div>
+        </li>
+    </ul>
+    
+<script>
+        function lightbox(idx) {
+            //show the slider's wrapper: this is required when the transitionType has been set to "slide" in the ninja-slider.js
+            var ninjaSldr = document.getElementById("ninja-slider");
+            ninjaSldr.parentNode.style.display = "block";
+
+            nslider.init(idx);
+
+            var fsBtn = document.getElementById("fsBtn");
+            fsBtn.click();
+        }
+
+        function fsIconClick(isFullscreen, ninjaSldr) { //fsIconClick is the default event handler of the fullscreen button
+            if (isFullscreen) {
+                ninjaSldr.parentNode.style.display = "none";
+            }
+        }
+    </script>
+</div>
